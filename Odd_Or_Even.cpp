0090d0a16a1e2a98c23a471b1390c8f2f842481a
the-one-samjones <theonesamjones@gmail.com>
2025-03-03 14:46:34 -0600

    Secondcommit

--- Odd_Or_Even.cpp
+++ Odd_Or_Even.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main(){
     int userIn;
 
-    cout << "Enter a number: ";
-    cin >> userIn;
+    cout << "Enter a number: ";       //Prompt user to enter any number
+    cin >> userIn;                    //Gets the integer entered by user
 
-    if (userIn % 2 == 0) {
-        cout << userIn << " is totally an even number." << endl;
+    if (userIn % 2 == 0) {            // Figures out whether the integer is divisible by 2 with no remainder (must be even number)
+        cout << userIn << " is totally an even number." << endl;    //Outputs the number and that the number is even
     }
-    else {
-        cout << userIn << " is DEFINITELY an odd number." << endl;
+    else{                         //If it is an integer but is not divisible by 2 with no remainder, it must be odd
+        cout << userIn << " is DEFINITELY an odd number." << endl;      //Outputs the number and that the number is odd
     }
     return 0;
 }
